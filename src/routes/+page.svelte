a6c2f95ecae4ee4505cfb12b16e82da0939d408c
Shivan Sivakumaran <51132467+ShivanS93@users.noreply.github.com>
2023-03-08 09:53:17 +1300

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	const handleSubmit = (e: Event & { currentTarget: HTMLFormElement }) => {
+		const formData = new FormData(e.currentTarget);
+		const data: Record<string, unknown> = {};
+
+		for (let field of formData) {
+			const [key, value] = field;
+			data[key] = value;
+		}
+
+		console.log(data);
+	};
+</script>
+
+<h1>Tarot Card Reader</h1>
+<form on:submit|preventDefault={handleSubmit}>
+	<label>
+		<strong>Question</strong>
+		<input type="text" id="question" />
+	</label>
+	<label>
+		Number of cards:
+		<select id="cardNumber">
+			<option>1</option>
+			<option>2</option>
+			<option>3</option>
+			<option>4</option>
+		</select>
+	</label>
+	<button aria-label="submit" type="submit">Submit</button>
+</form>

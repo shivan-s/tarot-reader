06448423851e386f28d27a988cc0985e318fe386
Shivan Sivakumaran <51132467+ShivanS93@users.noreply.github.com>
2023-03-10 20:22:12 +1300

    fixing the cards

--- src/components/tarotCard.svelte
+++ src/components/tarotCard.svelte
@@ -10,10 +10,10 @@
 	</figure>
 	<div class="card-body">
 		<h3 class="card-title text-center">{card.name}</h3>
-		<label for="my-modal-4" class="btn">Read more</label>
-		<input type="checkbox" id="my-modal-4" class="modal-toggle" />
-		<label for="my-modal-4" class="modal cursor-pointer">
-			<label class="modal-box relative max-5xl:" for="">
+		<label for={`card-model-${card.number}`} class="btn">Read more</label>
+		<input type="checkbox" id={`card-model-${card.number}`} class="modal-toggle" />
+		<label for={`card-modal-${card.number}`} class="modal cursor-pointer">
+			<label class="modal-box relative max-5xl" for={`card-model-${card.number}`}>
 				<div class="flex flex-wrap">
 					<h2 class="text-center">{card.name}</h2>
 					<figure class="px-5 pt-5">

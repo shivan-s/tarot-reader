5206a7b444e1f2bb47aef193b590156128a7a039
Shivan Sivakumaran <51132467+ShivanS93@users.noreply.github.com>
2023-03-10 13:40:27 +1300

    wip

--- src/components/tarotCard.svelte
+++ src/components/tarotCard.svelte
@@ -9,18 +9,18 @@
 		<img src={`/cards/${card.img}`} alt={card.name} />
 	</figure>
 	<div class="card-body">
-		<h5 class="card-title">{card.name}</h5>
+		<h3 class="card-title text-center">{card.name}</h3>
 		<label for="my-modal-4" class="btn">Read more</label>
 		<input type="checkbox" id="my-modal-4" class="modal-toggle" />
 		<label for="my-modal-4" class="modal cursor-pointer">
 			<label class="modal-box relative max-5xl:" for="">
 				<div class="flex flex-wrap">
-					<h2 class="text-lg font-bold">{card.name}</h2>
+					<h2 class="text-center">{card.name}</h2>
 					<figure class="px-5 pt-5">
 						<img src={`/cards/${card.img}`} alt={card.name} />
 					</figure>
 					<div class="flex flex-col">
-						<p>{card['Mythical/Spiritual']}</p>
+						<p>{card['Mythical/Spiritual'] || ''}</p>
 						<ul>
 							<strong>Fortune telling</strong>
 							{#each card.fortune_telling as fortune_telling}
